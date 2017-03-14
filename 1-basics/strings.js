6569462c119840d97cb4755341d232e0881deaa8
Olga Engels <ovengels@yahoo.com>
2017-03-13 23:29:57 -0500

    Update HTML

--- 1-basics/strings.js
+++ 1-basics/strings.js
@@ -1,9 +1,11 @@
 // 1 - Create a variable named phrase and assign it the value of an empty string
-
+var phrase = "";
 // 2 - Update the phrase variable by assigning it a value of "The rain in Spain falls gently on the plain"
-
+var phrase = "The rain in Spain falls gently on the plain";
 // 3 - Console log the length of phrase
-
-// 4 - Console log the word Spain by using the substring method
-
-// 5 - Append the value ". The end." to the phrase variable. 
+console.log(phrase.length);
+// 4 - Console log the word Spain by using c
+var word = "Spain";
+console.log(word.substring(0, 3));
+// 5 - Append the value ". The end." to the phrase variable.
+phrase.append("The end");
